36acbe496acd64c676d32105e233c92b5cc0b74b
yutae <42912205+y00913@users.noreply.github.com>
2021-04-04 02:06:16 +0900

    Update tcpecho_server.c

--- socket/tcpecho_server.c
+++ socket/tcpecho_server.c
@@ -6,48 +6,57 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
-#define MAXLINE  127
+#define MAXLINE 127
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     struct sockaddr_in servaddr, cliaddr;
-    int listen_sock, accp_sock,
-       addrlen=sizeof(cliaddr),
-       nbyte;
-  char buf[MAXLINE+1];
+    int listen_sock, accp_sock,  //소켓 번호
+        addrlen = sizeof(cliaddr),  //주소구조체 길이
+        nbyte;
+    char buf[MAXLINE + 1];  //수신 버퍼
 
-    if(argc != 2) {
+    if (argc != 2)
+    {
         printf("usage: %s port\n", argv[0]);
         exit(0);
-  }
-
-    if((listen_sock = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
-      perror("socket fail");
-      exit(0);
     }
 
-    bzero((char *)&servaddr, sizeof(servaddr));
+    if ((listen_sock = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("socket fail");
+        exit(0);
+    }
+
+    bzero((char *)&servaddr, sizeof(servaddr));  // servaddr을 '\0'으로 초기화
 
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(atoi(argv[1]));
 
-    if(bind(listen_sock, (struct sockaddr *)&servaddr,sizeof(servaddr)) < 0) {
-      perror("bind fail");
+    if (bind(listen_sock, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
+        perror("bind fail");
         exit(0);
     }
 
+    //소켓을 수동 대기모드로 세팅
     listen(listen_sock, 5);
 
-    while(1) {
+    //iterative 에코 서비스 수행
+    while (1)
+    {
         puts("waiting..");
 
+	//연결요청을 
         accp_sock = accept(listen_sock, (struct sockaddr *)&cliaddr, &addrlen);
-        if(accp_sock < 0) {
+        if (accp_sock < 0)
+        {
             perror("accept fail");
             exit(0);
         }
-        puts("connect client..");        
-	nbyte = read(accp_sock, buf, MAXLINE);
+        puts("connect client..");
+        nbyte = read(accp_sock, buf, MAXLINE);
         write(accp_sock, buf, nbyte);
         close(accp_sock);
     }
