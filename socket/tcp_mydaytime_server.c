fc669ded64ecd7c6e839543e82610ad9bc94d146
yutae <42912205+y00913@users.noreply.github.com>
2021-04-04 02:28:46 +0900

    Create tcp_mydaytime_server.c

---
+++ socket/tcp_mydaytime_server.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <time.h>
+
+#define MAXLINE  127
+
+int main(int argc, char *argv[]) {
+    struct sockaddr_in servaddr, cliaddr;
+    int listen_sock, accp_sock,
+       addrlen=sizeof(cliaddr),
+       nbyte;
+  char buf[MAXLINE+1];
+
+    if(argc != 2) {
+        printf("usage: %s port\n", argv[0]);
+        exit(0);
+  }
+
+    if((listen_sock = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+      perror("socket fail");
+      exit(0);
+    }
+
+    bzero((char *)&servaddr, sizeof(servaddr));
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(atoi(argv[1]));
+
+    if(bind(listen_sock, (struct sockaddr *)&servaddr,sizeof(servaddr)) < 0) {
+      perror("bind fail");
+        exit(0);
+    }
+
+    listen(listen_sock, 5);
+
+    while(1) {
+        puts("waiting..");
+
+        accp_sock = accept(listen_sock, (struct sockaddr *)&cliaddr, &addrlen);
+        if(accp_sock < 0) {
+            perror("accept fail");
+            exit(0);
+        }
+        puts("connect client..");
+        
+        //read()없이 현재 시간을 buf에 담아 write()함수로 클라이언트에 보냄
+        time_t now = time(NULL);
+        struct tm *t = localtime(&now);
+        strftime( buf, sizeof(buf), "%c", t);
+        nbyte = sizeof(buf);
+
+        write(accp_sock, buf, nbyte);
+        close(accp_sock);
+    }
+    close(listen_sock);
+    return 0;
+}
